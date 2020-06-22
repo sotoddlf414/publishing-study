9417e81ba4b1ccb162b2a0f786c7c15a763ca474
kimjunsang <sotoddlf414@naver.com>
2020-06-22 21:44:21 +0900

    jquery nav

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 $(function () {
 
+$('.picture-box img').click(function(){
+    
+    $('.picture-box').find('img').removeClass('active');
+    $(this).addClass('active');
+
+   var index = $('.picture-box').find('.active').index();
+   
+   $('.text-box').siblings().removeClass('active'); 
+   $('.text-box').eq(index).addClass('active') 
+    
+
+})
+
 
 
 });
