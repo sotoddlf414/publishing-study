8c4aacf32271854e8d854def56569cba50d15020
kimjunsang <sotoddlf414@naver.com>
2020-06-22 09:15:32 +0900

    css loading circle

--- js/main.js
+++ js/main.js
@@ -1,11 +1,5 @@
 $(function () {
 
-$('.menu li').mouseenter(function(){
-    $(this).children('.sub-menu').stop().slideDown(800);
-})
-$('.menu li').mouseleave(function(){
-    $(this).children('.sub-menu').stop().slideUp(800);
-})
 
 
 });
